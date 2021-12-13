6955ec4947ad7151441b6783e9c0d7d738a86a08
fatmakaroui <fatma.karoui@celevertech-france.fr>
2021-12-13 12:24:01 +0100

    fix style

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import '../styles/button.scss';
+import '../styles/status.scss';
 interface Props {
-  status:boolean;
-  onClick:()=>void;
+  status: boolean;
+  onClick: () => void;
 }
 
-const Status: React.FC<Props> = ({ 
-status, 
-onClick
-  }) => { 
+const Status: React.FC<Props> = ({
+  status,
+  onClick
+}) => {
   return (
-     <div className="status" style={{ background:status?'green':'red' ,cursor: 'pointer'}} onClick={onClick}>
-         {
-             status && <p style={{ color:'white',margin:'0px'}}>Complété</p>
-         }
-          {
-             !status && <p style={{ color:'white',margin:'0px'}}>Non complété</p>
-         }
-     </div>
+    <div className="status" style={{ background: status ? 'green' : 'red', cursor: 'pointer' }} onClick={onClick}>
+      {
+        status && <p className="text">Complété</p>
+      }
+      {
+        !status && <p className="text">Non complété</p>
+      }
+    </div>
   );
-  };
+};
 
 export default Status;
--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,52 +1,52 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import '../styles/task.scss';
-import { Button,Status,AddTask } from "./index";
+import { Button, Status, AddTask } from "./index";
 //Redux
 import { AppDispatch } from '../redux/store';
 import { useDispatch } from 'react-redux';
-import { removeTask,setTaskStatus } from '../redux/slices/taskSlice';
+import { removeTask, setTaskStatus } from '../redux/slices/taskSlice';
 import { Task } from "../shared/Models/Task";
 
 interface Props {
   data: Task;
 }
 
-const ToDoTask: React.FC<Props> = ({ 
-data, 
-  }) => { 
+const ToDoTask: React.FC<Props> = ({
+  data,
+}) => {
 
-    const [modelVisible,setModelVisible]=useState(false)
+  const [modelVisible, setModelVisible] = useState(false)
 
-    const handleModel=()=>{
-           setModelVisible(!modelVisible)
-    }
-  
-    const dispatch = useDispatch<AppDispatch>();
+  const handleModel = () => {
+    setModelVisible(!modelVisible)
+  }
 
-    
+  const dispatch = useDispatch<AppDispatch>();
+
+
   return (
-      <>
-     
-   <div className="containerTesk" > 
-      <div className="topTask">
+    <>
+
+      <div className="containerTesk" >
+        <div className="topTask">
           <div className="taskData">
-            <p>{data.name}</p>
-            <p>{data.description}</p>
+            <p className="name">{data.name}</p>
+            <p className="description">{data.description}</p>
           </div>
-         <Status status={data.status} onClick={()=>{dispatch(setTaskStatus({status:!data.status,id:data.id}))}}/>
+          <Status status={data.status} onClick={() => { dispatch(setTaskStatus({ status: !data.status, id: data.id })) }} />
+        </div>
+        <div className="bottomTask">
+          <Button name='Supprimer' onClick={() => { dispatch(removeTask(data.id)) }} color={'#060000'} radius={'0px'} />
+          <Button name='Modifier' onClick={() => { handleModel() }} color={'#060000'} radius={'0px'} />
+
+        </div>
+
       </div>
-      <div className="bottomTask">
-      <Button name='Supprimer' onClick={()=>{dispatch(removeTask(data.id))}} color={'#060000'} radius={'0px'}/>
-      <Button name='Modifier' onClick={()=>{handleModel()}} color={'#060000'} radius={'0px'}/>
-   
-      </div>
-    
-   </div>
-     {
-        modelVisible && <AddTask onClick={()=>{handleModel()}} description={data.description} name={data.name} id={data.id}/>
-    }
-     </>
+      {
+        modelVisible && <AddTask onClick={() => { handleModel() }} description={data.description} name={data.name} id={data.id} />
+      }
+    </>
   );
-  };
+};
 
 export default ToDoTask;
